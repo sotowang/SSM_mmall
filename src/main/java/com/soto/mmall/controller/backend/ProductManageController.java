05e6979a3c237c22eb143b3356b079c767ca2b6a
sotowang <sotowang@qq.com>
2018-07-21 00:16:51 +0800

    product接口: 6.richtext upload

--- src/main/java/com/soto/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/soto/mmall/controller/backend/ProductManageController.java
@@ -10,6 +10,7 @@ import com.soto.mmall.service.IFileService;
 import com.soto.mmall.service.IProductService;
 import com.soto.mmall.service.IUserService;
 import com.soto.mmall.util.PropertiesUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -108,17 +110,61 @@ public class ProductManageController {
             return ServerResponse.createByErrorMessage("需要管理员权限");
         }
     }
+
     @RequestMapping("upload.do")
     @ResponseBody
-    public ServerResponse upload(MultipartFile file, HttpServletRequest request) {
-        String path = request.getSession().getServletContext().getRealPath("upload");
-        String targetFileName = iFileService.upload(file, path);
-        String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
-        Map fileMap = Maps.newHashMap();
-        fileMap.put("uri", targetFileName);
-        fileMap.put("url", url);
+    public ServerResponse upload(HttpSession session, @RequestParam(value = "upload_file", required = false) MultipartFile file, HttpServletRequest request) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登陆,请选登陆");
+        }
+        //校验是否为管理员
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file, path);
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+            Map fileMap = Maps.newHashMap();
+            fileMap.put("uri", targetFileName);
+            fileMap.put("url", url);
+            return ServerResponse.createBySuccess(fileMap);
+        }else {
+            return ServerResponse.createByErrorMessage("需要管理员权限");
+        }
 
-        return ServerResponse.createBySuccess(fileMap);
+    }
+
+    @RequestMapping("richtext_img_upload.do")
+    @ResponseBody
+    public Map richtextImgUpload(HttpSession session, HttpServletResponse response, @RequestParam(value = "upload_file", required = false) MultipartFile file, HttpServletRequest request) {
+        Map resultMap = Maps.newHashMap();
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            resultMap.put("success", false);
+            resultMap.put("msg", "请登陆管理员");
+            return resultMap;
+        }
+        //父文本中对返回值有自己的要求,我们使用的是simditor
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            String path = request.getSession().getServletContext().getRealPath("upload");
+            String targetFileName = iFileService.upload(file, path);
+            if (StringUtils.isBlank(targetFileName)) {
+                resultMap.put("success", false);
+                resultMap.put("msg", "上传失败");
+                return resultMap;
+            }
+            String url = PropertiesUtil.getProperty("ftp.server.http.prefix") + targetFileName;
+
+            resultMap.put("success", true);
+            resultMap.put("msg", "上传成功");
+            resultMap.put("file_path", url);
+            response.addHeader("Access-Control-Allow-Headers","X-File-Name");
+            return resultMap;
+        } else {
+            resultMap.put("success", false);
+            resultMap.put("msg", "无权限操作");
+            return resultMap;
+        }
+
     }
 
 }
